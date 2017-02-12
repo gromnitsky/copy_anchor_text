c6c355dba3481328f1a5d90fd757f19c6fa0da88
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2017-02-12 20:05:44 +0200

    add img tags support

--- src/content_script.js
+++ src/content_script.js
@@ -1,15 +1,23 @@
 /* global chrome */
 
-let MyAnchor = null
+let CapturedData = null
 
 // listen to a message from event_page.js
 chrome.extension.onMessage.addListener(function(req, sender, sendRes) {
     if (req !== "menuClick")
 	throw new Error('invalid message from event_page.js: ' + req)
 
-    sendRes(MyAnchor)
+    if (CapturedData === null) {
+	sendRes({ err: "Contextmenu event didn't fire, blame the web page!" })
+	return
+    }
+
+    sendRes({ text: CapturedData })
+    CapturedData = null
 })
 
+// a capturing event
 document.addEventListener('contextmenu', function(event) {
-    MyAnchor = event.target.innerText
+    let n = event.target
+    CapturedData = n.innerText || n.title || n.alt
 }, true)
--- src/event_page.js
+++ src/event_page.js
@@ -8,21 +8,28 @@ let clipboard_write = function(str) {
 }
 
 let click = function(info, tab) {
-    let msg = 'Failed to extract the anchor text'
+    let complain = (msg) => alert("Failed to extract the text:\n\n" + msg)
 
     // send a message to content_script.js
     chrome.tabs.sendMessage(tab.id, "menuClick", (res) => {
 	if (chrome.runtime.lastError) {
-	    alert(`${msg}: ${chrome.runtime.lastError.message}`)
-	    return
-	}
-	if ( !(res && res.trim().length)) {
-	    alert(msg)
+	    complain(chrome.runtime.lastError.message
+		    + "\n\nReload the page & retry.")
 	    return
 	}
 
-	console.log(res.length, `${res.slice(0, 9)}…`)
-	clipboard_write(res)
+	res = res || {}
+	if (res.err) {
+	    complain(res.err)
+	    return
+	}
+	if ( !(res.text && res.text.trim().length)) {
+	    complain('No useful data in the attributes.')
+	    return
+	}
+
+	console.log(res.text.length, `${res.text.slice(0, 9)}…`)
+	clipboard_write(res.text)
     })
 }
 
@@ -32,10 +39,10 @@ document.body.appendChild(document.createElement("textarea"))
 
 // the callback shouldn't run each time chrome wakes up the extension
 chrome.runtime.onInstalled.addListener(() => {
-    console.info('creating a menu item')
+    console.info('add a menu item')
     chrome.contextMenus.create({
 	"id": "0",
 	"title": "֎",
-	"contexts": ["link"]
+	"contexts": ["link", "image"]
     })
 })
